ed12392be2be539831ae4c09205d54e5cdb9c1c6
Jarosław Frydrych <jarek.frydrych@gmail.com>
2016-09-08 11:54:30 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var http = require('http');
+var express = require('express');
+var app = express();
+
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end("hello world!\n");
+});
+
+app.get('/asd', function(req, res) {
+    res.send('hello world');
+});
+
+server.listen(3000);
