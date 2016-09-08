a1eb316de6418aca88e97c31a25db9bfa530aa74
Jarosław Frydrych <jarek.frydrych@gmail.com>
2016-09-08 12:34:42 +0200

    express

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-var http = require('http');
 var express = require('express');
 var app = express();
 
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end("hello worldsaaa!\n");
+app.get('/', function (req, res) {
+    res.send('Hello World!');
 });
 
-app.get('/asd', function(req, res) {
-    res.send('hello world');
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
 });
-
-server.listen(3000);
