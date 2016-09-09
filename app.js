6663306ff78da0af9f09f407716e64cb6c86dfbf
Jarosław Frydrych <jarek.frydrych@gmail.com>
2016-09-09 12:50:20 +0200

    express

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-/*var http = require('http');
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end("hello world!\n");
-});*/
-
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -16,9 +10,12 @@ var users = require('./routes/users');
 
 var app = express();
 
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -28,9 +25,38 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
-/*app.get('/', function(req, res){
-    res.send('hello world');
-});*/
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
 
 app.listen(3000);
-//server.listen(3000);
