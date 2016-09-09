8371b5a03533063cd0781861edb1b6230a68457f
Jarosław Frydrych <jarek.frydrych@gmail.com>
2016-09-09 12:15:32 +0200

    stuff

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
-var http = require('http');
+/*var http = require('http');
 var server = http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end("hello world!\n");
-});
-
+});*/
 
 var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
 var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
     res.send('hello world');
