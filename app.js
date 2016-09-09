97845469fe4d325b04e20a03534f7eab8eb6b43e
Jarosław Frydrych <jarek.frydrych@gmail.com>
2016-09-09 14:22:34 +0200

    user connected

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ io.on('connection', function(socket){
     console.log('a user connected');
     socket.on('disconnect', function(){
         console.log('user disconnected');
+        io.emit('user connected', 'user connected');
     });
 
     socket.on('chat message', function(msg){
